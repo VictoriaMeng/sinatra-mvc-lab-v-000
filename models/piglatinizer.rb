ad66bf6599299bad2e7d8abbe03f74d9a5f57793
Victoria Meng <nights1ststar@protonmail.com>
2018-05-24 01:25:55 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,5 +1,8 @@
 class PigLatinizer
+  
   def initialize
+    blends = %w(bl br ch d cr dr fl fr gl gr pl pr sc sh sk sl sm sn sp st sw th tr tw wh wr sch scr shr sph spl spr squ str thr)
+    @blends = blends + words.map { |word| word.capitalize }
   end
 
   def piglatinize(word)
